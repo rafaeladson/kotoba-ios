48f383c9e09945131f2e5ab36d7e8b4f45f2050e
rafael.adson <rafael@fiive.net>
2011-09-28 18:01:19 -0300

    Criação do protótipo do novo projeto.

---
+++ Kotoba/QuestionGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  QuestionGameViewController.h
+//  Kotoba
+//
+//  Created by Rafael Adson Barbosa Barros on 9/24/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuestionGameViewController : UIViewController
+
+@end
